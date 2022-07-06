0b5cb623eeaa0cb4663515ef4c6e07fad1dfc65d
Bastien Deye <Basdeye@gmail.com>
2022-07-06 16:07:33 +0200

    YT Course -- form WIP (time code 3h24)

--- front/react-pokemon-app/src/App.tsx
+++ front/react-pokemon-app/src/App.tsx
@@ -3,28 +3,31 @@ import PokemonList from "./pages/pokemon-list";
 import PokemonDetail from "./pages/pokemon-detail";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import PageNotFound from "./pages/page-not-found";
+import PokemonEdit from "./pages/pokemon-edit";
+
 
 const App: React.FunctionComponent = () => {
 
-        return  (
-            <Router>
-                   <div>
-                           {/* bar de nav */}
-                           <nav className="nav-wrapper teal">
-                                <Link to="/" className="brand-logo center">
-                                        Pokédex
-                                </Link>
-                           </nav>
-                           {/*Systeme de gestion des routes de l'appli*/}
-                           <Switch>
-                                   <Route exact path="/" component={PokemonList}/>
-                                   <Route exact path="/pokemons" component={PokemonList}/>
-                                   <Route path="/pokemons/:id" component={PokemonDetail}/>
-                                   <Route component={PageNotFound}/>
-                           </Switch>
-                   </div>
-            </Router>
-        )
+    return (
+        <Router>
+            <div>
+                {/* bar de nav */}
+                <nav className="nav-wrapper teal">
+                    <Link to="/" className="brand-logo center">
+                        Pokédex
+                    </Link>
+                </nav>
+                {/*Systeme de gestion des routes de l'appli*/}
+                <Switch>
+                    <Route exact path="/" component={PokemonList}/>
+                    <Route exact path="/pokemons" component={PokemonList}/>
+                    <Route exact path="/pokemons/edit/:id" component={PokemonEdit}/>
+                    <Route path="/pokemons/:id" component={PokemonDetail}/>
+                    <Route component={PageNotFound}/>
+                </Switch>
+            </div>
+        </Router>
+    )
 }
 
 export default App;
